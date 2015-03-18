a7c4c52b947b1bf3fce90d575feef0aaddf40302
dfw10011 <sunjianhua1978@163.com>
2015-03-18 17:38:14 +0800

    新增文件

---
+++ LayersToCSD.jsx
@@ -0,0 +1,519 @@
+﻿// This script exports photoshop layers as individual PNGs. It also
+// writes a JSON file that can be imported into Spine where the images
+// will be displayed in the same positions and draw order.
+
+// Setting defaults.
+var writePngs = true;
+var writeTemplate = false;
+var writeJson = true;
+var ignoreHiddenLayers = true;
+var pngScale = 1;
+var groupsAsSkins = false;
+var useRulerOrigin = false;
+var imagesDir = "./images/";
+var projectDir = "";
+var padding = 1;
+
+// IDs for saving settings.
+const settingsID = stringIDToTypeID("settings");
+const writePngsID = stringIDToTypeID("writePngs");
+const writeTemplateID = stringIDToTypeID("writeTemplate");
+const writeJsonID = stringIDToTypeID("writeJson");
+const ignoreHiddenLayersID = stringIDToTypeID("ignoreHiddenLayers");
+const groupsAsSkinsID = stringIDToTypeID("groupsAsSkins");
+const useRulerOriginID = stringIDToTypeID("useRulerOrigin");
+const pngScaleID = stringIDToTypeID("pngScale");
+const imagesDirID = stringIDToTypeID("imagesDir");
+const projectDirID = stringIDToTypeID("projectDir");
+const paddingID = stringIDToTypeID("padding");
+
+var originalDoc;
+try {
+	originalDoc = app.activeDocument;
+} catch (ignored) {}
+var settings, progress;
+loadSettings();
+showDialog();
+
+function tenNum()
+{
+	return '1' + Math.floor((Math.random() * 9 + 1) * 0x10000000)
+      .toString(10)
+      .substring(1);
+}
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
+
+function run () {
+	// Output dirs.
+	var absProjectDir = absolutePath(projectDir);
+	new Folder(absProjectDir).create();
+	var absImagesDir = absolutePath(imagesDir);
+	var imagesFolder = new Folder(absImagesDir);
+	imagesFolder.create();
+	var relImagesDir = imagesFolder.getRelativeURI(absProjectDir);
+	relImagesDir = relImagesDir == "." ? "" : (relImagesDir + "/");
+
+	// Get ruler origin.
+	var xOffSet = 0, yOffSet = 0;
+	if (useRulerOrigin) {
+		var ref = new ActionReference();
+		ref.putEnumerated(charIDToTypeID("Dcmn"), charIDToTypeID("Ordn"), charIDToTypeID("Trgt"));
+		var desc = executeActionGet(ref);
+		xOffSet = desc.getInteger(stringIDToTypeID("rulerOriginH")) >> 16;
+		yOffSet = desc.getInteger(stringIDToTypeID("rulerOriginV")) >> 16;
+	}
+
+	activeDocument.duplicate();
+
+	// Output template image.
+	if (writeTemplate) {
+		if (pngScale != 1) {
+			scaleImage();
+			storeHistory();
+		}
+
+		var file = new File(absImagesDir + "template");
+		if (file.exists) file.remove();
+
+		activeDocument.saveAs(file, new PNGSaveOptions(), true, Extension.LOWERCASE);
+
+		if (pngScale != 1) restoreHistory();
+	}
+
+	if (!writeJson && !writePngs) {
+		activeDocument.close(SaveOptions.DONOTSAVECHANGES);
+		return;
+	}
+
+	// Rasterize all layers.
+	try {
+		executeAction(stringIDToTypeID( "rasterizeAll" ), undefined, DialogModes.NO);
+	} catch (ignored) {}
+
+	// Collect and hide layers.
+	var layers = [];
+	collectLayers(activeDocument, layers);
+	var layersCount = layers.length;
+
+	storeHistory();
+
+	// Store the slot names and layers for each skin.
+	var slots = {}, skins = { "default": [] };
+	for (var i = layersCount - 1; i >= 0; i--) {
+		var layer = layers[i];
+
+		// Use groups as skin names.
+		var potentialSkinName = trim(layer.parent.name);
+		var layerGroupSkin = potentialSkinName.indexOf("-NOSKIN") == -1;
+		var skinName = (groupsAsSkins && layer.parent.typename == "LayerSet" && layerGroupSkin) ? potentialSkinName : "default";
+
+		var skinLayers = skins[skinName];
+		if (!skinLayers) skins[skinName] = skinLayers = [];
+		skinLayers[skinLayers.length] = layer;
+
+		slots[layerName(layer)] = true;
+	}
+
+	//
+	var docName = decodeURI(originalDoc.name);
+		docName = docName.substring(0, docName.indexOf("."));
+	var tagIndex = 74;
+	var docWidth = activeDocument.width.as("px") * pngScale;
+	var	docHeight = activeDocument.height.as("px") * pngScale;
+
+	// Output skeleton and bones.
+	var json = '<GameProjectFile>\n';
+	json += '\t<PropertyGroup Type="Scene" Name="' + docName + '" ID="' + guid() + '" Version="2.1.5.0" />\n';
+	json += '\t<Content ctype="GameProjectContent">\n';
+	json += '\t\t<Content>\n';
+	json += '\t\t<Animation Duration="0" Speed="1.0000" />\n';
+	json += '\t\t<ObjectData Name="Scene" FrameEvent="" Tag="' + tagIndex + '" ctype="SingleNodeObjectData">\n';
+	tagIndex += 1;
+	json += '\t\t\t<Position X="0.0000" Y="0.0000" />\n';
+	json += '\t\t\t<Scale ScaleX="1.0000" ScaleY="1.0000" />\n';
+	json += '\t\t\t<AnchorPoint />\n';
+	json += '\t\t\t<CColor A="255" R="255" G="255" B="255" />\n';
+	json += '\t\t\t<Size X="' + docWidth + '" Y="' + docHeight + '" />\n';
+	json += '\t\t\t<PrePosition X="0.0000" Y="0.0000" />\n';
+	json += '\t\t\t<PreSize X="0.0000" Y="0.0000" />\n';
+	json += '\t\t\t<Children>\n';
+
+	// Output slots.
+	// var slotsCount = countAssocArray(slots);
+	// var slotIndex = 0;
+	// for (var slotName in slots) {
+	// 	if (!slots.hasOwnProperty(slotName)) continue;
+
+	// 	// Use image prefix if slot's attachment is in the default skin.
+	// 	var attachmentName = slotName;
+	// 	var defaultSkinLayers = skins["default"];
+	// 	for (var i = defaultSkinLayers.length - 1; i >= 0; i--) {
+	// 		if (layerName(defaultSkinLayers[i]) == slotName) {
+	// 			attachmentName = slotName;
+	// 			break;
+	// 		}
+	// 	}
+
+	// 	json += '\t{"name":"' + slotName + '","bone":"root","attachment":"' + attachmentName + '"}';
+	// 	slotIndex++;
+	// 	json += slotIndex < slotsCount ? ",\n" : "\n";
+	// }
+	// json += '],\n"skins":{\n';
+
+	// Output skins.
+	var skinsCount = countAssocArray(skins);
+	var skinIndex = 0;
+	for (var skinName in skins) {
+		if (!skins.hasOwnProperty(skinName)) continue;
+		// json += '\t"' + skinName + '":{\n';
+
+		var skinLayers = skins[skinName];
+		var skinLayersCount = skinLayers.length;
+		var skinLayerIndex = 0;
+		// for (var i = skinLayersCount - 1; i >= 0; i--) {
+		for (var i = 0; i < skinLayersCount; i++) {
+			var layer = skinLayers[i];
+			var slotName = layerName(layer);
+			var placeholderName, attachmentName;
+			if (skinName == "default") {
+				placeholderName = slotName;
+				attachmentName = placeholderName;
+			} else {
+				placeholderName = slotName;
+				attachmentName = skinName + "/" + slotName;
+			}
+
+			var x = activeDocument.width.as("px") * pngScale;
+			var y = activeDocument.height.as("px") * pngScale;
+
+			layer.visible = true;
+			if (!layer.isBackgroundLayer) activeDocument.trim(TrimType.TRANSPARENT, false, true, true, false);
+			x -= activeDocument.width.as("px") * pngScale;
+			y -= activeDocument.height.as("px") * pngScale;
+			if (!layer.isBackgroundLayer) activeDocument.trim(TrimType.TRANSPARENT, true, false, false, true);
+			var width = activeDocument.width.as("px") * pngScale + padding * 2;
+			var height = activeDocument.height.as("px") * pngScale + padding * 2;
+
+			// Save image.
+			if (writePngs) {
+				if (pngScale != 1) scaleImage();
+				if (padding > 0) activeDocument.resizeCanvas(width, height, AnchorPosition.MIDDLECENTER);
+
+				if (skinName != "default") new Folder(absImagesDir + skinName).create();
+				activeDocument.saveAs(new File(absImagesDir + attachmentName), new PNGSaveOptions(), true, Extension.LOWERCASE);
+			}
+
+			restoreHistory();
+			layer.visible = false;
+
+			x += Math.round(width) / 2;
+			y += Math.round(height) / 2;
+
+			// Make relative to the Photoshop document ruler origin.
+			if (useRulerOrigin) {
+				x -= xOffSet * pngScale;
+				y -= activeDocument.height.as("px") * pngScale - yOffSet * pngScale; // Invert y.
+			}
+
+			json += '\t\t\t\t<NodeObjectData Name="' + slotName + '" ActionTag="' + tenNum() + '" FrameEvent="" Tag="' + tagIndex + '" ctype="SpriteObjectData">\n';
+			tagIndex += 1;
+			// json += '\t\t\t\t\t<Position X="' + (x - Math.round(width) * 0.5000) + '" Y="' + (y - Math.round(height) * 0.5000) + '" />\n';
+			json += '\t\t\t\t\t<Position X="' + (x) + '" Y="' + (y) + '" />\n';
+            json += '\t\t\t\t\t<Scale ScaleX="1.0000" ScaleY="1.0000" />\n';
+            json += '\t\t\t\t\t<AnchorPoint ScaleX="0.5000" ScaleY="0.5000" />\n';
+            json += '\t\t\t\t\t<CColor A="255" R="255" G="255" B="255" />\n';
+            json += '\t\t\t\t\t<Size X="' + Math.round(width) + '" Y="' + Math.round(height) + '" />\n';
+            json += '\t\t\t\t\t<PrePosition X="0.0000" Y="0.0000" />\n';
+            json += '\t\t\t\t\t<PreSize X="0.0000" Y="0.0000" />\n';
+            json += '\t\t\t\t\t<FileData Type="Normal" Path="' + relImagesDir + placeholderName + '.png" />\n';
+			json += '\t\t\t\t</NodeObjectData>\n';
+			// if (attachmentName == placeholderName) {
+				// json += '\t\t"' + slotName + '":{"' + placeholderName + '":{'
+					// + '"x":' + x + ',"y":' + y + ',"width":' + Math.round(width) + ',"height":' + Math.round(height) + '}}';
+			// } else {
+				// json += '\t\t"' + slotName + '":{"' + placeholderName + '":{"name":"' + attachmentName + '", '
+					// + '"x":' + x + ',"y":' + y + ',"width":' + Math.round(width) + ',"height":' + Math.round(height) + '}}';
+			// }
+
+			skinLayerIndex++;
+			// json += skinLayerIndex < skinLayersCount ? ",\n" : "\n";
+		}
+		//json += "\t\}";
+
+		skinIndex++;
+		// json += skinIndex < skinsCount ? ",\n" : "\n";
+	}
+	//json += '},\n"animations":{"animation":{}}\n}';
+	json += '\t\t\t</Children>\n';
+    json += '\t\t</ObjectData>\n';
+    json += '\t\t</Content>\n';
+	json += '\t</Content>\n';
+	json += '</GameProjectFile>\n';
+
+	activeDocument.close(SaveOptions.DONOTSAVECHANGES);
+
+	// Output JSON file.
+	if (writeJson) {
+		var name = decodeURI(originalDoc.name);
+		name = name.substring(0, name.indexOf("."));
+		var file = new File(absProjectDir + name + ".csd");
+		file.remove();
+		file.open("w", "TEXT");
+		file.lineFeed = "\n";
+		file.write(json);
+		file.close();
+	}
+}
+
+// Dialog and settings:
+
+function showDialog () {
+	if (!originalDoc) {
+		alert("Please open a document before running the LayersToPNG script.");
+		return;
+	}
+	if (!hasFilePath()) {
+		alert("Please save the document before running the LayersToPNG script.");
+		return;
+	}
+
+	var dialog = new Window("dialog", "Layers To Cocos Studio 2 CSD");
+	dialog.alignChildren = "fill";
+
+	var checkboxGroup = dialog.add("group");
+		var group = checkboxGroup.add("group");
+			group.orientation = "column";
+			group.alignChildren = "left";
+			var writePngsCheckbox = group.add("checkbox", undefined, " Write layers as PNGs");
+			writePngsCheckbox.value = writePngs;
+			var writeTemplateCheckbox = group.add("checkbox", undefined, " Write a template PNG");
+			writeTemplateCheckbox.value = writeTemplate;
+			var writeJsonCheckbox = group.add("checkbox", undefined, " Write Spine JSON");
+			writeJsonCheckbox.value = writeJson;
+		group = checkboxGroup.add("group");
+			group.orientation = "column";
+			group.alignChildren = "left";
+			var ignoreHiddenLayersCheckbox = group.add("checkbox", undefined, " Ignore hidden layers");
+			ignoreHiddenLayersCheckbox.value = ignoreHiddenLayers;
+			var groupsAsSkinsCheckbox = group.add("checkbox", undefined, " Use groups as skins");
+			groupsAsSkinsCheckbox.value = groupsAsSkins;
+			var useRulerOriginCheckbox = group.add("checkbox", undefined, " Use ruler origin as 0,0");
+			useRulerOriginCheckbox.value = useRulerOrigin;
+
+	var slidersGroup = dialog.add("group");
+		group = slidersGroup.add("group");
+			group.orientation = "column";
+			group.alignChildren = "right";
+			group.add("statictext", undefined, "PNG scale:");
+			group.add("statictext", undefined, "Padding:");
+		group = slidersGroup.add("group");
+			group.orientation = "column";
+			var scaleText = group.add("edittext", undefined, pngScale * 100);
+			scaleText.characters = 4;
+			var paddingText = group.add("edittext", undefined, padding);
+			paddingText.characters = 4;
+		group = slidersGroup.add("group");
+			group.orientation = "column";
+			group.add("statictext", undefined, "%");
+			group.add("statictext", undefined, "px");
+		group = slidersGroup.add("group");
+			group.alignment = ["fill", ""];
+			group.orientation = "column";
+			group.alignChildren = ["fill", ""];
+			var scaleSlider = group.add("slider", undefined, pngScale * 100, 1, 100);
+			var paddingSlider = group.add("slider", undefined, padding, 0, 4);
+	scaleText.onChanging = function () { scaleSlider.value = scaleText.text; };
+	scaleSlider.onChanging = function () { scaleText.text = Math.round(scaleSlider.value); };
+	paddingText.onChanging = function () { paddingSlider.value = paddingText.text; };
+	paddingSlider.onChanging = function () { paddingText.text = Math.round(paddingSlider.value); };
+
+	var outputGroup = dialog.add("panel", undefined, "Output directories");
+		outputGroup.alignChildren = "fill";
+		outputGroup.margins = [10,15,10,10];
+		var textGroup = outputGroup.add("group");
+			group = textGroup.add("group");
+				group.orientation = "column";
+				group.alignChildren = "right";
+				group.add("statictext", undefined, "Images:");
+				group.add("statictext", undefined, "JSON:");
+			group = textGroup.add("group");
+				group.orientation = "column";
+				group.alignChildren = "fill";
+				group.alignment = ["fill", ""];
+				var imagesDirText = group.add("edittext", undefined, imagesDir);
+				var projectDirText = group.add("edittext", undefined, projectDir);
+		outputGroup.add("statictext", undefined, "Begin paths with \"./\" to be relative to the PSD file.").alignment = "center";
+
+	var group = dialog.add("group");
+		group.alignment = "center";
+		var runButton = group.add("button", undefined, "OK");
+		var cancelButton = group.add("button", undefined, "Cancel");
+		cancelButton.onClick = function () {
+			dialog.close(0);
+			return;
+		};
+
+	function updateSettings () {
+		writePngs = writePngsCheckbox.value;
+		writeTemplate = writeTemplateCheckbox.value;
+		writeJson = writeJsonCheckbox.value;
+		ignoreHiddenLayers = ignoreHiddenLayersCheckbox.value;
+		var scaleValue = parseFloat(scaleText.text);
+		if (scaleValue > 0 && scaleValue <= 100) pngScale = scaleValue / 100;
+		groupsAsSkins = groupsAsSkinsCheckbox.value;
+		useRulerOrigin = useRulerOriginCheckbox.value;
+		imagesDir = imagesDirText.text;
+		projectDir = projectDirText.text;
+		var paddingValue = parseInt(paddingText.text);
+		if (paddingValue >= 0) padding = paddingValue;
+	}
+
+	dialog.onClose = function() {
+		updateSettings();
+		saveSettings();
+	};
+	
+	runButton.onClick = function () {
+		if (scaleText.text <= 0 || scaleText.text > 100) {
+			alert("PNG scale must be between > 0 and <= 100.");
+			return;
+		}
+		if (paddingText.text < 0) {
+			alert("Padding must be >= 0.");
+			return;
+		}
+		dialog.close(0);
+
+		var rulerUnits = app.preferences.rulerUnits;
+		app.preferences.rulerUnits = Units.PIXELS;
+		try {
+			run();
+		} catch (e) {
+			alert("An unexpected error has occurred.\n\nTo debug, run the LayersToPNG script using Adobe ExtendScript "
+				+ "with \"Debug > Do not break on guarded exceptions\" unchecked.");
+			debugger;
+		} finally {
+			if (activeDocument != originalDoc) activeDocument.close(SaveOptions.DONOTSAVECHANGES);
+			app.preferences.rulerUnits = rulerUnits;
+		}
+	};
+
+	dialog.center();
+	dialog.show();
+}
+
+function loadSettings () {
+	try {
+		settings = app.getCustomOptions(settingsID);
+	} catch (e) {
+		saveSettings();
+	}
+	if (typeof settings == "undefined") saveSettings();
+	settings = app.getCustomOptions(settingsID);
+	if (settings.hasKey(writePngsID)) writePngs = settings.getBoolean(writePngsID);
+	if (settings.hasKey(writeTemplateID)) writeTemplate = settings.getBoolean(writeTemplateID);
+	if (settings.hasKey(writeJsonID)) writeJson = settings.getBoolean(writeJsonID);
+	if (settings.hasKey(ignoreHiddenLayersID)) ignoreHiddenLayers = settings.getBoolean(ignoreHiddenLayersID);
+	if (settings.hasKey(pngScaleID)) pngScale = settings.getDouble(pngScaleID);
+	if (settings.hasKey(groupsAsSkinsID)) groupsAsSkins = settings.getBoolean(groupsAsSkinsID);
+	if (settings.hasKey(useRulerOriginID)) useRulerOrigin = settings.getBoolean(useRulerOriginID);
+	if (settings.hasKey(imagesDirID)) imagesDir = settings.getString(imagesDirID);
+	if (settings.hasKey(projectDirID)) projectDir = settings.getString(projectDirID);
+	if (settings.hasKey(paddingID)) padding = settings.getDouble(paddingID);
+}
+
+function saveSettings () {
+	var settings = new ActionDescriptor();
+	settings.putBoolean(writePngsID, writePngs);
+	settings.putBoolean(writeTemplateID, writeTemplate);
+	settings.putBoolean(writeJsonID, writeJson);
+	settings.putBoolean(ignoreHiddenLayersID, ignoreHiddenLayers);
+	settings.putDouble(pngScaleID, pngScale);
+	settings.putBoolean(groupsAsSkinsID, groupsAsSkins);
+	settings.putBoolean(useRulerOriginID, useRulerOrigin);
+	settings.putString(imagesDirID, imagesDir);
+	settings.putString(projectDirID, projectDir);
+	settings.putDouble(paddingID, padding);
+	app.putCustomOptions(settingsID, settings, true);
+}
+
+// Photoshop utility:
+
+function scaleImage () {
+	var imageSize = activeDocument.width.as("px");
+	activeDocument.resizeImage(UnitValue(imageSize * pngScale, "px"), null, null, ResampleMethod.BICUBICSHARPER);
+}
+
+var historyIndex;
+function storeHistory () {
+	historyIndex = activeDocument.historyStates.length - 1;
+}
+function restoreHistory () {
+	activeDocument.activeHistoryState = activeDocument.historyStates[historyIndex];
+}
+
+function collectLayers (layer, collect) {
+	for (var i = 0, n = layer.layers.length; i < n; i++) {
+		var child = layer.layers[i];
+		if (ignoreHiddenLayers && !child.visible) continue;
+		if (child.bounds[2] == 0 && child.bounds[3] == 0) continue;
+		if (child.layers && child.layers.length > 0)
+			collectLayers(child, collect);
+		else if (child.kind == LayerKind.NORMAL) {
+			collect.push(child);
+			child.visible = false;
+		}
+	}
+}
+
+function hasFilePath () {
+	var ref = new ActionReference();
+	ref.putEnumerated(charIDToTypeID("Dcmn"), charIDToTypeID("Ordn"), charIDToTypeID("Trgt"));
+	return executeActionGet(ref).hasKey(stringIDToTypeID("fileReference"));
+}
+
+function absolutePath (path) {
+	path = trim(path);
+	if (path.length == 0)
+		path = activeDocument.path.toString();
+	else if (imagesDir.indexOf("./") == 0)
+		path = activeDocument.path + path.substring(1);
+	path = path.replace(/\\/g, "/");
+	if (path.substring(path.length - 1) != "/") path += "/";
+	return path;
+}
+
+// JavaScript utility:
+
+function countAssocArray (obj) {
+	var count = 0;
+	for (var key in obj)
+		if (obj.hasOwnProperty(key)) count++;
+	return count;
+}
+
+function trim (value) {
+	return value.replace(/^\s+|\s+$/g, "");
+}
+
+function endsWith (str, suffix) {
+	return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
+
+function stripSuffix (str, suffix) {
+	if (endsWith(str.toLowerCase(), suffix.toLowerCase())) str = str.substring(0, str.length - suffix.length);
+	return str;
+}
+
+function layerName (layer) {
+	return stripSuffix(trim(layer.name), ".png").replace(/[:\/\\*\?\"\<\>\|]/g, "");
+}
--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# PhotoshopLayerToCocosStudioCSD
-用Photoshop自动创建CocosStudio场景文件CSD
+# 用Photoshop自动创建CocosStudio界面场景
+
+工欲善其事必先利其器，趁手的工具会让生活美好许多，虽然Cocos2d-x，提供了Cocos Studio 2界面编辑器，但是在美术设计好界面后，要在Cocos Studio 2界面编辑器呈现出来，确是个工作量比较大的体力活，首先美术要把界面元素切成小图，然后界面编辑器使用人员，根据效果图把美术提供的小图在编辑器中拼接出来，这几步操作非但没有技术含量，又特别费工时，于是就有了‘PhotoshopLayerToCocosStudioCSD’这个工具，让美术和界面编辑器使用人员，可以节省大量时间去上上网，聊聊天。
+
+#工具介绍
+
+用Photoshop自动创建CocosStudio界面场景的工具，是：LayersToCSD.jsx，这是一个photoshop脚本文件，安装和使用非常方便，这个photoshop脚本，改编自Spine的LayersToPNG.jsx脚本，只是修改了里面的文件输出部分，所以会看到操作界面是一样的。
+
+这个工具的调试用的是：Adobe Photoshop CS6，其它版本没测试过。
+
+输出的csd文件格式为Cocos Studio 2的v2.1.5版本，其它版本没测试过。
+
+##安装
+安装非常简单，只要下载LayersToCSD.jsx，然后把文件放到photoshop的‘Scripts’目录就可以了。
+
+photoshop的‘Scripts’目录详细路径：photoshop安装盘\photoshop文件夹\Presets\Scripts\
+
+##使用
+使用非常简单，只要两步：
+
+1. 在photoshop中，打开要输出的文件，选择photoshop的‘文件’菜单，再选择‘脚本’子菜单就可以看到‘LayersToCSD’这个子菜单，点击，弹出操作窗口，如果没什么修改，直接点‘ok'按钮。</br>![image](README/show1.png)
+
+2. 把上一步生成的.csd文件和images文件，拖拽到Cocos Studio 2界面编辑器已打开的工程。</br>![image](README/show2.png)
+
+经过上面两步操作，又可以继续刷微信，看淘宝了。
+
+#附加信息
+1. 因为要在photoshop中，把每个层转换为图片存储到磁盘，这个操作会慢一些，在开始执行后，稍等一下，会看到在photoshop中出现一些额外的操作窗口，等这些额外的操作窗口，自动执行完全部操作，并自动关闭，转换就全部结束了，如果都是按默认操作执行，可以到.psd文件存放目录，看到新增加了一个.csd文件和一个images文件夹（这个操作都是自动操作，不用干预）。
+
+2. 用这个工具生成的.csd文件，‘NodeObjectData’都为’SpriteObjectData‘，如果能在Cocos Studio 2中可视化修改对象类型就方便多了。
+
+3. 如果想测试工具方便性，手头又没有.psd文件，在[https://github.com/sunjianhua/PhotoshopLayerToCocosStudioCSD](https://github.com/sunjianhua/PhotoshopLayerToCocosStudioCSD)，提供了一个’skills.psd‘文件，这个文件来自网络免费资源。
+
+4. 这个工具可以完善的地方比较多，比如美术人员在photoshop设计好以后，执行脚本，可以直接打开Cocos Studio 2看在编辑器效果，不过没什么实用价值，让美术额外装一个比较难用的Cocos Studio 2，还是放弃吧。
+
+5. 这个工具可以完善的地方比较多，比如可以在photoshop的Layer增加些关键字，可以直接用工具生成Cocos Studio 2编辑器用到的‘按钮’、‘复选框、‘输入框’等控件，让美术做额外的工作，貌似也不太好，放弃。
+
+6. 这个工具不支持中文，因为Spine的LayersToPNG.jsx脚本就不支持，哈哈。
+
+7. 这个工具根据.psd的文件名，生成.csd的文件名。
+
+8. 这个工具根据photoshop中Layer的名字，来生成图片的名字，所以这个要策划先把命名规则给美术沟通好，游戏用图的命名规则，就是photoshop中Layer的命名规则，因为不支持中文，所以如果photoshop中Layer名为中文，那么最终输出的图片名为乱码？？？
+
+9. 这个工具可能没后续完善，希望Cocos Studio 2编辑器开发方能出一个完善的photoshop插件。
+
+10. 给美术推荐方便在真机上看设计效果的利器：[ps play](http://isux.tencent.com/app/psplay)，这是别人开发的，有问题就问别人。
+
+11. 最后附上完整操作视频。
+
